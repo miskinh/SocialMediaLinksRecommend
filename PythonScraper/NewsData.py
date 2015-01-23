050c73f1934c8fb3a3deab508a290d3ecde79337
Henry Miskin <henry.miskin@talktalk.net>
2015-01-23 14:57:16 +0000

    Completed refactor

--- PythonScraper/NewsData.py
+++ PythonScraper/NewsData.py
@@ -2,4 +2,21 @@ from News.ArticleScraper import ArticleScraper
 from Return import Return
 from Update import Update
 
-article = ArticleScraper()
+def updateLinks(minLength=100):
+  "updates all links in the database"
+
+  articleScraper = ArticleScraper()
+  returnData = Return()
+  updateData = Update()
+
+  urls = returnData.getDocumentURLs()
+
+  for url in urls:
+    article = articleScraper.getURL(url)
+
+    if (len(article.text) < minLength):
+      updateData.removeLink(url)
+    else:
+      updateData.updateLink(url,article.title,article.text)
+
+
