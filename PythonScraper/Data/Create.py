c97f31e07724580822847be6260be1b7a6dc7dce
Henry Miskin <henry.miskin@talktalk.net>
2015-01-25 20:47:06 +0000

    Commented Create.py and removed dissused functions

--- PythonScraper/Data/Create.py
+++ PythonScraper/Data/Create.py
@@ -2,13 +2,22 @@ import datetime
 from Models import *
 
 class Create(object):
-  """docstring for Create"""
+  """Create is used to add entities to the database as defined in Models.py"""
 
   def __init__(self):
     database.connect()
 
   def addUserObject(self,user):
-    "adds a user object"
+    """Adds a User to the database along with user relations and post urls
+
+    Parameters:
+    user -- dictionary containing the following user parameters
+      - userName
+      - followers
+      - friends
+      - postURLs
+      - likeURLs
+    """
 
     self.addUser(
       userName = user["userName"],
@@ -19,7 +28,20 @@ class Create(object):
       )
 
   def addUser(self,userName,friendUserNames=[],followerUserNames=[],postURLs=[],likeURLs=[]):
-    "add user adds the given user to the database"
+    """Adds a User to the database, depending on the optional parameters also adds
+    friends, followers, posted content and liked content. For any existing item in the
+    database relations will be added for the existing entity
+
+    Parameters:
+      userName - userName of the user being added
+      friendUserNames - list of userNames of people being followed by the user
+      followerUserNames - list of userNames of people following the given user 
+      postURLs - list of URLs posted by the given user
+      likeURLs - list of URLs where the user has liked the comment containing the URL
+
+    Returns:
+      user - a User object of the added user as defined in Models.py
+    """
 
     #Check if user exists else create
     try:
@@ -31,22 +53,31 @@ class Create(object):
     for followerUserName in followerUserNames:
       self.addFollow(followerUserName,userName)
 
-    #Loop followers and add relationships
+    #Loop friends and add relationships
     for friendUserName in friendUserNames:
       self.addFollow(userName,friendUserName)
 
-    #Loop documentURLs and add
+    #Loop postURLs and add
     for postURL in postURLs:
       self.addPost(userName,postURL)
 
-    #Loop documentURLs and add
+    #Loop likeURLs and add
     for likeURL in likeURLs:
       self.addLike(userName,likeURL)
 
     return user
 
   def addFollow(self,fromUserName,toUserName):
-    "adds a following relationship between two user "
+    """Adds a follow relationship from a given user to another as defined by 
+    their userName, if either user does not exist then new users are created.
+
+    Parameters:
+      fromUserName - the userName of the follower
+      toUserName - the userName of the followed
+
+    Returns:
+      follow - a Following object for the relationship as defined in Models.py
+    """
 
     fromUser = self.addUser(fromUserName)
     toUser = self.addUser(toUserName)
@@ -66,7 +97,15 @@ class Create(object):
     return follow
 
   def addDocument(self,documentURL):
-    "adds a document to the database"
+    """Adds a document with the given URL to the database, if a document with the
+    given URL exists then the existing document is returned
+
+    Parameters:
+      documentURL - the URL of the document being added to the database
+
+    Returns:
+      document - a Document object as defined in Models.py
+    """
 
     try:
       document = Document.select().where(Document.url == documentURL)[0]
@@ -76,7 +115,17 @@ class Create(object):
     return document
 
   def addPost(self,userName,documentURL):
-    "adds a post of a document to the database"
+    """Adds a post relationship to the database where the given userName has posted
+    the given URL, if the user or document does not exist then they are added to the
+    database
+
+    Parameters:
+      userName - the userName of the user that posted the document
+      documentURL - the URL that has been posted
+
+    Returns:
+      post - a Posting object that has been added to the database
+    """
 
     user = self.addUser(userName)
     document = self.addDocument(documentURL)
@@ -89,8 +138,20 @@ class Create(object):
     except DoesNotExist:
       post = Posting.create(document = document, user=user)
 
+    return post
+
   def addLike(self,userName,documentURL):
-    "adds a like of a ocument to the database"
+    """Adds a like relationship to the database where the given userName has liked
+    the given URL, if the user or document does not exist then they are added to the
+    database
+
+    Parameters:
+      userName - the userName of the user that has done the liking
+      documentURL - the URL that has been liked
+
+    Returns:
+      like - a Liking object that has been added to the database
+    """
 
     user = self.addUser(userName)
     document = self.addDocument(documentURL)
@@ -102,6 +163,8 @@ class Create(object):
       )
     except DoesNotExist:
       like = Liking.create(document = document, user=user)
+
+    return like
 
 
 if (__name__ == "__main__"):
